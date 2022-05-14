2b63cf5a5dc629cd6ca6f171e0a0cfb39590ca14
guimaraaes <sguimaraaes@gmail.com>
2022-05-14 15:44:36 -0300

    feat: implementing select_user in UserRepository and test it

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,6 +1,7 @@
 # pylint: disable=F1101
 
 from collections import namedtuple
+from typing import List
 
 from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
@@ -29,4 +30,35 @@ class UserRepository:
                 raise
             finally:
                 db_connection.session.close()
+        return None
+
+    @classmethod
+    def select_user(clc, user_id=None, name: str = None) -> List[Users]:
+        """
+        Select data in user entity by id and/or name
+        :param - user_id: Id of the registry
+                - name: User name
+        :return - List with Users selected
+        """
+        try:
+            query_data = None
+            if user_id and not name:
+
+                with DBConnectionHandler() as db_connection:
+                    data = db_connection.session.query(UsersModel).filter_by(id=user_id).one()
+                    query_data = [data]
+            elif not user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = db_connection.session.query(UsersModel).filter_by(name=name).one()
+                    query_data = [data]
+            elif user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = db_connection.session.query(UsersModel).filter_by(id=user_id, name=name).one()
+                    query_data = [data]
+            return query_data
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()
         return None
