1fa27809b409296853eb6d1c5896d410ef058a01
guimaraaes <sguimaraaes@gmail.com>
2022-06-03 17:03:28 -0300

    feat: implementing findUser case and test

---
+++ src/domain/use_cases/find_user.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractclassmethod
+from typing import Dict, List
+
+from src.domain.models import Users
+
+
+class FindUser(ABC):
+    """interface to FindPet use case"""
+
+    @abstractclassmethod
+    def by_id(cls, user_id: int) -> Dict[bool, List[Users]]:
+        """Specifc case"""
+        raise Exception("should implement method: by_id")
+
+    @abstractclassmethod
+    def by_name(cls, name: str) -> Dict[bool, List[Users]]:
+        """Specifc case"""
+        raise Exception("should implement method: by_name")
+
+    @abstractclassmethod
+    def by_id_and_name(cls, user_id: int, name: str) -> Dict[bool, List[Users]]:
+        """Specifc case"""
+        raise Exception("should implement method: by_id_and_name")
