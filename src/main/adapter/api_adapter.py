901327908f87f770e3150cedf47a96f8b6b14a56
guimaraaes <sguimaraaes@gmail.com>
2022-06-06 14:10:38 -0300

    feat: implementing register_user route with flask

---
+++ src/main/adapter/api_adapter.py
@@ -0,0 +1,22 @@
+from sqlite3 import IntegrityError
+from typing import Type
+
+from src.main.interface import RouteInterface as Route
+from src.presenters.errors import HttpErrors
+from src.presenters.helpers import HttpRequest, HttpResponse
+
+
+def flask_adapter(request: any, api_route: Type[Route]) -> any:
+    """Adapter pattern to flask
+    :param - flask request
+    :api_route - composite routes
+    """
+    http_request = HttpRequest(body=request.json)
+    try:
+        response = api_route.route(http_request)
+    except IntegrityError:
+        http_error = HttpErrors.error_409()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
+    return response
